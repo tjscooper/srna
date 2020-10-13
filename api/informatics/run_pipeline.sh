6e4376b8f6b7b753ae8c0e682788336b0a7624f4
dfox3 <fox.dylan92@gmail.com>
2020-10-13 09:20:06 -0500

    building pipeline - init of json updating and progress arithmetic

--- api/informatics/run_pipeline.sh
+++ api/informatics/run_pipeline.sh
@@ -4,6 +4,12 @@ now=$(date +"%Y%m%d.%H%M%S%3N")
 touch reports/$1
 ref_name=$1
 shift
+i=0
+num_files=${#@}
+num_steps=$(( 7*num_files + 2 ))
+echo "Number of steps: $num_steps"
+
+jq -c --arg var1 "$ref_name" '. { "\($var1)": { "state": "Initializing", "progress": "0" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json pipeline_status.json
 
 for file in "$@" 
 do
