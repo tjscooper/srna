e3028f16d7af3adf341ec9f679e9317ce26a7f88
dfox3 <fox.dylan92@gmail.com>
2020-10-13 09:51:43 -0500

    building pipeline - init of json updating part 2

--- api/informatics/run_pipeline.sh
+++ api/informatics/run_pipeline.sh
@@ -9,7 +9,7 @@ num_files=${#@}
 num_steps=$(( 7*num_files + 2 ))
 echo "Number of steps: $num_steps"
 
-jq -c --arg var1 "$ref_name" '. + { "\($var1)": { "state": "Initializing", "progress": "0" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json pipeline_status.json
+jq -c --arg var1 "$ref_name" '. + { "\($var1)": { "state": "Initializing", "progress": "0" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json
 #rm public/json/tmp*.json
 
 for file in "$@" 
@@ -19,7 +19,7 @@ do
 	#cutadapt
 	((i++))
 	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-	jq -c --arg var1 "$ref_name" --arg var2 "$progress" --arg var3 "$file" '."\($var1)" = { "state": "Trimming \($var3)", "progress": "\($var2)" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json pipeline_status.json
+	jq -c --arg var1 "$ref_name" --arg var2 "$progress" --arg var3 "$file" '."\($var1)" = { "state": "Trimming \($var3)", "progress": "\($var2)" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json
 
     echo "Aligning with bowtie2"
 
