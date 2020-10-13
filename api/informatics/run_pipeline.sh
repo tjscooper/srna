71d4c74866bbce9eeeab1781073c8a2f3a83e2a1
dfox3 <fox.dylan92@gmail.com>
2020-10-13 09:44:54 -0500

    building pipeline - init of json updating part 2

--- api/informatics/run_pipeline.sh
+++ api/informatics/run_pipeline.sh
@@ -10,11 +10,16 @@ num_steps=$(( 7*num_files + 2 ))
 echo "Number of steps: $num_steps"
 
 jq -c --arg var1 "$ref_name" '. + { "\($var1)": { "state": "Initializing", "progress": "0" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json pipeline_status.json
+rm public/json/tmp*.json
 
 for file in "$@" 
 do
     echo "File: $file";
     echo "Trimming with cutadapt"
+	#cutadapt
+	((i++))
+	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
+	jq -c --arg var1 "$ref_name" --arg var2 "$progress" --arg var3 "$file" '. + { "\($var1)": { "state": "Trimming \($var3)", "progress": "\($var2)" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json pipeline_status.json
 
     echo "Aligning with bowtie2"
 
