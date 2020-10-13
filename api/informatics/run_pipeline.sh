f059db82d77597ddc8e729f61f932a0a9b28f4d9
dfox3 <fox.dylan92@gmail.com>
2020-10-13 09:03:56 -0500

    building pipeline - comments added

--- api/informatics/run_pipeline.sh
+++ api/informatics/run_pipeline.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
+
 now=$(date +"%Y%m%d.%H%M%S%3N")
-sleep 5
 touch reports/$1
 ref_name=$1
 shift
@@ -7,5 +8,23 @@ shift
 for file in "$@" 
 do
     echo "File: $file";
+    echo "Trimming with cutadapt"
+
+    echo "Aligning with bowtie2"
+
+    echo "Converting sam to bam with samtools"
+
+    echo "Indexing bam with samtools"
+
+    echo "Sorting bam with samtools"
+    
+    echo "Generating count stats with samtools"
+
+    echo "Removing residual files"
+    echo
    
 done
+
+echo "Archiving report files"
+
+echo "Final cleaning"
