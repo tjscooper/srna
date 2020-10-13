49de7df814939dedf48a677cf478b5b2af9fa85e
dfox3 <fox.dylan92@gmail.com>
2020-10-13 11:43:58 -0500

    building pipeline - adding samtools full pathing

--- api/informatics/run_pipeline.sh
+++ api/informatics/run_pipeline.sh
@@ -39,7 +39,7 @@ do
 
     echo "Converting sam to bam with samtools"
     #samtools
-    samtools view -bS public/$p.sam > public/$p.bam
+    /tmp/samtools-1.9/samtools view -bS public/$p.sam > public/$p.bam
 	((i++))
 	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
 	jq -c --arg var1 "$ref_name" --arg var2 "$progress" --arg var3 "$file" '."\($var1)" = { "state": "Sorting bams \($var3)", "progress": "\($var2)" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json
