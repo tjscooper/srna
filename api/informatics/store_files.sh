8ed6714ec0fa1a05dd257b5e421e3892bc2dc4b4
dfox3 <fox.dylan92@gmail.com>
2020-10-14 14:32:31 -0500

    s3 storage init - unix carriage returns

--- api/informatics/store_files.sh
+++ api/informatics/store_files.sh
@@ -2,6 +2,6 @@
 now=$(date +"%Y%m%d.%H%M%S%3N")
 for file in "$@" 
 do
-	aws s3 cp public/$1 s3://booshboosh/pipelinedata/$file
+	aws s3 cp public/$file s3://booshboosh/pipelinedata/$file
 	rm public/$file
 done
