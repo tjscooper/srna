6eb43bd2ca011396214f6161c9e68a91d349ea17
dfox3 <fox.dylan92@gmail.com>
2020-10-29 14:32:35 -0500

    force

--- api/informatics/run_pipeline.sh
+++ api/informatics/run_pipeline.sh
@@ -12,11 +12,11 @@ num_files=${#@}
 num_steps=$(( 8*num_files + 2 ))
 echo "Number of steps: $num_steps"
 
-#screen -S jq_qq_queue -X stuff "jq -c '. + [\"$ref_name\"]' public/json/queue.json > public/json/tmp2.$$.json && mv public/json/tmp.$$.json public/json/queue.json^M"
-#screen -S jq_pipe_queue -X stuff "jq -c '. + { \"$ref_name\": { \"state\": \"In Queue: Position $queue_pos\", \"progress\": \"0\" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json"
+#screen -S jq_qq_queue -X stuff "jq -c '. + [\"$ref_name\"]' public/json/queue.json > public/json/tmp2.$$.json && mv --force public/json/tmp.$$.json public/json/queue.json^M"
+#screen -S jq_pipe_queue -X stuff "jq -c '. + { \"$ref_name\": { \"state\": \"In Queue: Position $queue_pos\", \"progress\": \"0\" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json"
 #screen -S pipeline_queue -X stuff "$cmd"
 
-screen -S jq_pipe_queue -X stuff "jq -c '. + { \"$ref_name\": { \"state\": \"Initializing\", \"progress\": \"0\" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+screen -S jq_pipe_queue -X stuff "jq -c '. + { \"$ref_name\": { \"state\": \"Initializing\", \"progress\": \"0\" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
 #rm public/json/tmp*.json
 
 for file in "$@" 
@@ -28,12 +28,12 @@ do
 
     echo "Prefix: $p: Downloading from booshboosh s3";
     progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-    screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Accessing $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+    screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Accessing $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
 	aws s3 cp s3://booshboosh/pipelinedata/$f public/$f
 	aws s3 cp public/$f s3://booshboosh/pipelinedata/$f
 	((i++))
     progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-    screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Trimming \"$f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+    screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Trimming \"$f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
     
 	echo "Trimming with cutadapt"
 	../../miniconda3/bin/cutadapt -a TGGAATTCTCGGGTGCCAAGG -u 4 -u -4 -m 10 -o public/$p.trimmed.fastq.gz public/$f > public/$ref_name/$p.trim.txt
@@ -41,7 +41,7 @@ do
 	#cutadapt
 	((i++))
 	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Aligning $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Aligning $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
 	
     echo "Aligning with bowtie2"
 	#bowtie
@@ -49,7 +49,7 @@ do
     rm public/$p.trimmed.fastq.gz
 	((i++))
 	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Converting sams $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Converting sams $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
 
     echo "Converting sam to bam with samtools"
     #samtools
@@ -57,7 +57,7 @@ do
     rm public/$p.sam
 	((i++))
 	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Sorting bams $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Sorting bams $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
 
     echo "Sorting bam with samtools"
     #samtools
@@ -65,21 +65,21 @@ do
     rm public/$p.bam
 	((i++))
 	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Indexing bams $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Indexing bams $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
 
     echo "Indexing bam with samtools"
     #samtools
     ../../samtools-1.9/samtools index public/$p.sorted.bam
 	((i++))
 	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Generating counts $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Generating counts $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
     
     echo "Generating count stats with samtools"
     #samtools
     ../../samtools-1.9/samtools idxstats public/$p.sorted.bam > public/$ref_name/$p.counts.txt
 	((i++))
 	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Removing temporary files $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Removing temporary files $f\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
 
     echo "Removing residual files"
     echo
@@ -88,7 +88,7 @@ do
   	rm -f public/json/tmp*.json
 	((i++))
 	progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Preparing for next file\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+	screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Preparing for next file\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
    
 done
 
@@ -96,14 +96,14 @@ echo "Archiving report files"
 zip -r reports/$ref_name.zip public/$ref_name
 ((i++))
 progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Archiving run\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Archiving run\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
    
 echo "Final cleaning"
 rm -rf public/$ref_name
 ((i++))
 progress=$(bc -l <<< "scale=2;$i*100/$num_steps")
-screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Complete\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
-screen -S jq_qq_queue -X stuff "jq -c 'del( .[0] )' public/json/queue.json > public/json/tmp2.$$.json && mv public/json/tmp2.$$.json public/json/queue.json^M"
+screen -S jq_pipe_queue -X stuff "jq -c '.\"$ref_name\" = { \"state\": \"Complete\", \"progress\": \"$progress\" }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
+screen -S jq_qq_queue -X stuff "jq -c 'del( .[0] )' public/json/queue.json > public/json/tmp2.$$.json && mv --force public/json/tmp2.$$.json public/json/queue.json^M"
 
 q_ele=$(<public/json/queue.json jq -r '. | @sh')
 echo $q_ele
@@ -111,6 +111,6 @@ queue_pos=1
 for ele in $q_ele
 do
     echo $ele
-    screen -S jq_pipe_queue -X stuff "jq -c '. + { \"$ele\": { \"state\": \"In Queue: Position $queue_pos\", \"progress\": \"0\" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv public/json/tmp.$$.json public/json/pipeline_status.json^M"
+    screen -S jq_pipe_queue -X stuff "jq -c '. + { \"$ele\": { \"state\": \"In Queue: Position $queue_pos\", \"progress\": \"0\" } }' public/json/pipeline_status.json > public/json/tmp.$$.json && mv --force public/json/tmp.$$.json public/json/pipeline_status.json^M"
     ((i++))
 done
