e5a3e927542fe4a091ce2f1dcc77e8265ec27772
dfox3 <fox.dylan92@gmail.com>
2021-07-12 13:20:31 -0500

    listen to email 11

--- api/post_email.py
+++ api/post_email.py
@@ -43,7 +43,7 @@ def main():
     payload = {'email': str(options.e), 
                'link1': "<a href=\"" + str(options.l1) + "\">download</a>", 
                'link2': "<a href=\"" + str(options.l1) + "\">view data</a>" }
-    response = requests.post(final_url, data=json.dumps(payload))
+    response = requests.post(final_url, data=json.dumps(payload), headers={'Content-Type': 'application/json'})
 
     print(response.text) #TEXT/HTML
     print(response.status_code, response.reason) #HTTP
