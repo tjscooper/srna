4705fa9a03a8f20eabbf900f0d4941f618d8e0b2
dfox3 <fox.dylan92@gmail.com>
2021-07-12 19:36:26 -0500

    listen to email 24

--- api/post_email.py
+++ api/post_email.py
@@ -48,16 +48,19 @@ def main():
                <link href=\"https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lobster&display=swap\" rel=\"stylesheet\"> \
                </head>\
                <div style=\"width:100%; height:100%\" >\
-               <div style=\"background-color:#00A1DE; color:white; font-size:20px> \
+               <div style=\"background-color:#00A1DE; color:white; font-size:28px> \
                <br/> \
                ANALYSIS \
                <br/> \
                <br/> \
-               <div style=\"background-color:#54B245; color:white; font-size:20px> \
+               </div> \
+               <br/> \
+               <div style=\"background-color:#54B245; color:white; font-size:28px> \
                <br/> \
                COMPLETE \
                <br/> \
                <br/> \
+               </div> \
                <a style=\"color:white\" href=\"" + str(options.l1) + "\">download</a> \
                <br/> \
                <a style=\"color:white; line-height: 12px; \
