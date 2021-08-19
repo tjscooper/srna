d4513eb033d71912ab6727b2e6570c2d6d11fe11
U-PERKINELMERNET\GarzaA25518 <GarzaA25518@WALRL2114.PERKINELMER.NET>
2021-08-19 15:27:13 -0500

    full view fun 9

--- api/dibs_server.js
+++ api/dibs_server.js
@@ -85,7 +85,7 @@ app.post('/delete',function(req, res) {
 app.get('/fullview',function(req,res) {
   //executes a pipeline on currently uploaded file\
 
-    var cmd = "python3 dib3.py full_view"
+    var cmd = "python3 dibs/dib3.py full_view"
     child = exec(cmd,
     function (error, stdout, stderr) {
         if (error) {
